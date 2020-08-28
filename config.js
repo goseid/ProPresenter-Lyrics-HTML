0a5768d994cdd10feaaac5311f76299749960c04
Greg Oseid <3221311+goseid@users.noreply.github.com>
2020-08-28 12:19:40 -0500

    included config.js (previously ignored)

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {
+    "IPAddress": "10.1.10.249",
+    "IPPort": 53309,	// intially 50001
+    "Password": "123", //initially "password"
+
+    "SplitLines": null, // Set this to a string to allow multi-lingual slide splitting
+    "SplitLinesNum": 0, // Which version of the slide do you want to use? (hint: starts at zero)
+
+    "ClockLocale": "en-US", // Local to use for displaying the clock
+
+    // Un-implemented settings:
+
+    //"MergeLines": false,
+    //"MergeLinesMin": 4,
+    //"MergeLinesJoinChar": ",",
+
+    //"TimerLabels": ["Elapsed Time", "Video Countdown"]
+}
